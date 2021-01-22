22c722852c2ba0616e6a362248709c97a1a38f4f
SamukaAlves000 <60469274+SamukaAlves000@users.noreply.github.com>
2021-01-22 00:48:02 -0300

    Criando service de servico

--- src/app/modules/servico/servico.service.ts
+++ src/app/modules/servico/servico.service.ts
@@ -11,26 +11,26 @@ import { catchError } from 'rxjs/operators';
 export class ServicoService {
 
   private apiServer = 'http://localhost:8080';
-  private authorizationData = 'Basic ' + btoa('samuel' + ':' + 'samuel');
 
   httpOptions = {
     headers: new HttpHeaders({
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      Authorization: 'Basic ' + btoa('admin' + ':' + 'admin')
     })
   };
 
   constructor(private httpClient: HttpClient) { }
 
-
   getAll(): Observable<Servico[]> {
     return this.httpClient.get<Servico[]>(this.apiServer + '/servicos/admin/get', this.httpOptions)
       .pipe(
         catchError(this.errorHandler)
-      ); }
+      );
+  }
 
   errorHandler(error) {
     let errorMessage = '';
-    if(error.error instanceof ErrorEvent) {
+    if (error.error instanceof ErrorEvent) {
       // Get client-side error
       errorMessage = error.error.message;
     } else {
