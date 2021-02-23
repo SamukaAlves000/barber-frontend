6d3d9a324ad2935175b8ff605f8e1f8f4ea9f54f
SamukaAlves000 <60469274+SamukaAlves000@users.noreply.github.com>
2021-02-23 19:23:01 -0300

    Gerando horarios automaticamente

--- src/app/modules/agendamento/agendamento-create/agendamento-create.component.ts
+++ src/app/modules/agendamento/agendamento-create/agendamento-create.component.ts
@@ -1,7 +1,6 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Funcionario} from '../../funcionario/funcionario';
 import {FuncionarioService} from '../../funcionario/funcionario.service';
-import {Agendamento} from '../agendamento';
 import {Servico} from '../../servico/servico';
 import {ServicoService} from '../../servico/servico.service';
 import {Pessoa} from '../../pessoa/pessoa';
@@ -16,23 +15,20 @@ import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 })
 export class AgendamentoCreateComponent implements OnInit {
 
-  isLinear = false;
-  firstFormGroup: FormGroup;
-  secondFormGroup: FormGroup;
 
+  agendamentoForm: FormGroup;
   dateFormCtrl = new FormControl(new Date());
   current = new Date();
   minDate = new Date(this.current.getFullYear(), new Date().getMonth(), Number(this.current.toLocaleDateString().split('/')[0]));
   maxDate = new Date(this.current.getFullYear() , 11, 31);
   horario = undefined;
 
-  agendamento: Agendamento = {};
   horariosAgendados: string[] = [];
 
   funcionarios: Funcionario[];
   servicos: Servico[] = [];
   pessoas: Pessoa[] = [];
-  horarios = ['09:00:00', '10:00:00', '11:00:00', '12:00:00', '14:00:00', '15:00:00', '16:00:00', '17:00:00'];
+  horarios = [];
 
   constructor(private agendamentoService: AgendamentoService,
               private funcionarioService: FuncionarioService,
@@ -42,26 +38,22 @@ export class AgendamentoCreateComponent implements OnInit {
 
   ngOnInit(): void {
 
-    this.buscarHorariosAgendados();
-    this.gerarHorario();
-
-    this.funcionarioService.getAll().subscribe((data: Funcionario[]) => {
-      console.log(data);
-      this.funcionarios = data;
+    this.agendamentoForm = this.formBuilder.group({
+      dataAgendamento: [''],
+      horario: [''],
+      status: [''],
+      avaliacao: [''],
+      funcionario: [{}],
+      servico: [{}],
+      pessoa: [{}]
     });
 
-    this.servicoService.getAll().subscribe((data: Servico[]) => {
-      console.log(data);
-      this.servicos = data;
-    });
+    const  data = this.dateFormCtrl.value.toLocaleDateString().split('/');
+    this.gerarHorario(data);
 
-    this.firstFormGroup = this.formBuilder.group({
-      firstCtrl: ['', Validators.required]
+    this.dateFormCtrl.valueChanges.subscribe(value => {
+      this.gerarHorario(value.toLocaleDateString().split('/'));
     });
-    this.secondFormGroup = this.formBuilder.group({
-      secondCtrl: ['', Validators.required]
-    });
-
   }
 
   cancel(): void {
@@ -69,26 +61,26 @@ export class AgendamentoCreateComponent implements OnInit {
   }
 
   setFuncionario(funcionarioSelecionado: Funcionario): void{
-    this.agendamento.funcionario = funcionarioSelecionado;
+    this.agendamentoForm.value.funcionario = funcionarioSelecionado;
   }
 
   setServico(servicoSelecionado: Servico): void{
-    this.agendamento.servico = servicoSelecionado;
+    this.agendamentoForm.value.servico = servicoSelecionado;
   }
 
   setPessoa(pessoaSelecionada: Pessoa): void{
-    this.agendamento.pessoa = pessoaSelecionada;
+    this.agendamentoForm.value.pessoa = pessoaSelecionada;
   }
 
   setAgendamentoDataAgendamentoAndHorario(): void {
     const data = this.dateFormCtrl.value.toLocaleDateString().split('/');
-    this.agendamento.dataAgendamento = data[2] + '-' + data[1] + '-' + data[0];
-    this.agendamento.horario = this.agendamento.dataAgendamento + 'T' + this.horario + ':00';
+    this.agendamentoForm.value.dataAgendamento = data[2] + '-' + data[1] + '-' + data[0];
+    this.agendamentoForm.value.horario = this.agendamentoForm.value.dataAgendamento + 'T' + this.horario;
   }
 
   createAgendamento(): void {
     this.setAgendamentoDataAgendamentoAndHorario();
-    this.agendamentoService.create(this.agendamento).subscribe(res => {
+    this.agendamentoService.create(this.agendamentoForm.value).subscribe(res => {
         this.funcionarioService.showMessage('Agendamento CRIADO!');
         this.cancel();
       }
@@ -97,8 +89,7 @@ export class AgendamentoCreateComponent implements OnInit {
 
 
   cancel2(): void {
-      // this.buscarHorariosDisponiveis(null);
-    this.gerarHorario();
+     // alert('Data: ' + this.dateFormCtrl.value.toLocaleDateString());
   }
 
   setHorario(horario: string) {
@@ -112,9 +103,13 @@ export class AgendamentoCreateComponent implements OnInit {
     });
   }
 
-  gerarHorario(): void {
-    this.horariosAgendados.forEach(value => console.log(value.split('T')[1]));
-    this.horariosAgendados = this.horariosAgendados.map(value => value.split('T')[1]);
-    this.horarios = this.horarios.filter(value => this.horariosAgendados.indexOf(value) < 0);
+  gerarHorario(data): void{
+    this.horarios = ['09:00:00', '10:00:00', '11:00:00', '12:00:00', '14:00:00', '15:00:00', '16:00:00', '17:00:00'];
+    this.agendamentoService.getAllHorarios(data[2] + '-' + data[1] + '-' + data[0]).toPromise()
+      .then(value => {
+        this.horariosAgendados = value;
+        this.horariosAgendados = this.horariosAgendados.map(value1 => value1.split('T')[1]);
+        this.horarios = this.horarios.filter(value1 => this.horariosAgendados.indexOf(value1) < 0);
+      });
   }
 }
